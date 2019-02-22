8dba0f8a94f2781fe2131cf420a737aab074ac48
Nehomah Mora <nehomahm@gmail.com>
2019-02-22 09:29:05 -0700

    added constructor docblock and public function

--- php/classes/Todo.php
+++ php/classes/Todo.php
@@ -6,6 +6,7 @@ require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Fullstack\Assessment\ValidateDate;
 use Fullstack\Assessment\ValidateUuid;
+use http\Exception\InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -37,4 +38,31 @@ class Todo implements \JsonSerializable {
 	 * task for todo
 	 * @var string $todoTask;
 	 */
-}
+	private $todoTask;
+
+	/**
+	 * Constructor for Todo
+	 *
+	 * @param string|Uuid $newTodoId id for this todo, null if a new todo
+	 * @param string $newTodoAuthor is authors name
+	 * @param \DateTime|string $newTodoDate date of todo
+	 * @param string $newTodoTask info of task
+	 * @throws \InvalidArgumentException if data types are not InvalidArgumentException
+	 * @throws \RangeException if data values are out of bounds (strings too long, negative integers, etc)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 *
+	 **/
+	public function __construct($newTodoId, string $newTodoAuthor, $newTodoDate, string $newTodoTask) {
+				try {
+						$this->setTodoId($newTodoId);
+						$this->setTodoAuthor($newTodoAuthor);
+						$this->setTodoDate($newTodoDate);
+						$this->setTodoTask($newTodoTask);
+				} catch(\InvalidArgumentException | \RangeException | \TypeError | \Exception $exception) {
+						//determine what exception type was thrown
+						$exceptionType = get_class($exception);
+						throw(new $exceptionType($exception->getMessage(), 0, $exception));
+				}
+	}
