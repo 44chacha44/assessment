089dca4e125390d324a4ff80fca19ed68c03122d
Nehomah Mora <nehomahm@gmail.com>
2019-02-22 11:53:40 -0700

    tapping out, i dont know what to enter

--- php/lib/todo.php
+++ php/lib/todo.php
@@ -1,6 +1,14 @@
-<?php 
+<?php
+namespace  uss-hopper\assessment;
+
+use 
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once ("autoload.php");
+require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 //absolute path to YOUR ini file on the server 
 $secrets =  new Secrets("/etc/apache2/capstone-mysql/assessment.ini");
 $pdo = $secrets->getPdoObject();
+
+
+
