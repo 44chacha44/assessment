8ce5ae557fae972744c5fc3aae62a8f42254861e
Nehomah Mora <nehomahm@gmail.com>
2019-02-22 08:37:29 -0700

    added index and sql file incl table

---
+++ sql/data-design.sql
@@ -0,0 +1,10 @@
+ALTER	DATABASE ###### CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXITS todo;
+
+CREATE TABLE todo (
+	todoId BINARY(16) NOT NULL,
+	todoAuthor VARCHAR(32) NOT NULL,
+	todoDate DATETIME(6) NOT NULL,
+	todoTask VARCHAR(255)
+);
