44f61c6fbd4498530ca7d646a1acf3bc80f29dc2
Nehomah Mora <nehomahm@gmail.com>
2019-02-22 11:28:49 -0700

    added ini address

--- php/lib/todo.php
+++ php/lib/todo.php
@@ -2,5 +2,5 @@
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 //absolute path to YOUR ini file on the server 
-$secrets =  new Secrets("commit-push-and-ask-for-ini-file");
+$secrets =  new Secrets("/etc/apache2/capstone-mysql/assessment.ini");
 $pdo = $secrets->getPdoObject();
